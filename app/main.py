e66bd35cf158461e86f28b69eb8d583c324b63da
Alsabbagh Louai <144901821+louals@users.noreply.github.com>
2025-07-16 20:45:18 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.auth import router as auth_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()  
 
@@ -8,6 +9,16 @@ app = FastAPI(
     title="Auth Service",
     description="Microservice pour l'authentification (signup, login)",
     version="1.0.0"
+)
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or "*" to allow all for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Inclusion des routes
